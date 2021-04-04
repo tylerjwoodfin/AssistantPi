30efd1dae9c619ef3c0572d816a1b8a0222d50f2
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-04-03 23:24:55 -0500

    Added preliminary email function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const spawn = require("child_process").exec;
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -14,27 +15,40 @@ const myHome = new GoogleHome("192.168.1.225");
 myHome.speak("Hello Tyler!");
 
 function runServer() {
-  app.set('view engine', 'ejs');
+	app.set('view engine', 'ejs');
 
-  router.get('/test', function(req, res) {
-    console.log("Test");
-    myHome.speak("Looks good.");
-    res.end("Hello!!\n");
-  });
+	router.get('/', function(req, res) {
+		console.log(JSON.stringify(req.query));
 
-  router.get('/', function(req, res) {
-    console.log("FirstPage");
-    myHome.speak("Looks blank.");
-    res.end("Firstly");
-  });
+		if(req.query.say) {
+			myHome.speak(req.query.say);
+		}
+		
+		if(req.query.email) {
+			spawn(`python3 ~/Git/Tools/mail.py "${encodeURIComponent('TestSubject')}" "${encodeURIComponent(req.query.email)}"`, (err, stdout, stderr) => {
+				if (err) {
+					//some err occurred
+					console.error(err)
+				} else {
+					console.log(`${stdout}`);
+				}
+			});
+		}
+	
+		res.end("Enter something for me to say!");
+	});
 
-  // make the app listen for routes
-  app.use('/', router);
-  app.listen(process.env.port || 3000);
+	// make the app listen for routes
+	app.use('/', router);
+	app.listen(process.env.port || 3000);
 
-  console.log('Running at Port 3000');
+	console.log('Running at Port 3000');
 
-  // res.end("Hello World");
+	// res.end("Hello World");
+}
+
+function escapeQuotes(str) {
+	return str.replace(/"/g, '\\\"').replace(/'/g, '\\\'');
 }
 
 setTimeout(runServer,3000);
